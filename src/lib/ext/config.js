e8d0165a78438c73c0543aad2f94df85e1d6fa71
qodebot <30004919+qodebot@users.noreply.github.com>
2018-07-03 06:00:48 +0000

    update advanced flex, kinda messy

--- src/lib/ext/config.js
+++ src/lib/ext/config.js
@@ -12,6 +12,7 @@ $percs     = $config['percs']['vals'];
 //console.log($size_lbls)
 
 function * listGenerator(iter){
+  //console.info('list generator',iter)
   //let iter = devices;
   let index = 0;
   let names = (Array.isArray(iter)) ? iter : Object.keys(iter);
@@ -23,30 +24,58 @@ function * listGenerator(iter){
 
 
 //console.log(deviceGenerator)
-const deviceGen = listGenerator($devices);
+var deviceGen = listGenerator($devices);
 
 function makeDeviceGenerator(){
   const len =  Object.keys($devices).length;
-  return function(getLength){
-    if(getLength===1) return len;
-    let $this = deviceGen.next();
-    if( ! $this.done ) return $this.value; 
-    return null;
+  function restart(){
+    console.log('restarting generator...');
+    deviceGen = listGenerator($devices);
   }
+  function generate(getLength){
+    if(getLength===1) return len;
+    if(!deviceGen)    return restart;
+    let $this = deviceGen.next();
+    if( !$this.done ) return $this.value; 
+    if( $this.done ){
+      console.error('generator done, sending restart()');
+      return restart;
+    }
+  }
+  return generate; 
 }
 
 
-const sizeGen = listGenerator($size_lbls);
+
+var sizeGen = listGenerator($size_lbls);
 
 function makeSizeGenerator(){
   const len =  Object.keys($size_lbls).length;
-  return function(getLength){
-    //console.log('len',(getLength?))
-    if(getLength===1) return len;
-    let $this = sizeGen.next();
-    if( ! $this.done ) return ""+$this.value; 
-    return null;
+  function restart(){
+    //console.log('restarting generator...');
+    sizeGen = listGenerator($size_lbls);
   }
+  function generate(getLength){
+    if(getLength===1) return len;
+    if(!deviceGen)    return restart;
+    let $this = sizeGen.next();
+    if( !$this.done ) return ""+$this.value; 
+    if( $this.done ){
+      //console.error('generator done, sending restart()');
+      return restart;
+    }
+  }
+
+
+  return generate; 
+
+  // return function(getLength){
+  //   //console.log('len',(getLength?))
+  //   if(getLength===1) return len;
+  //   let $this = sizeGen.next();
+  //   if( ! $this.done ) return ""+$this.value; 
+  //   return null;
+  // }
 }
 
 
@@ -89,32 +118,56 @@ module.exports = () => {
     });
 
 
-    stylus.define('fx_devices', function(device,param) {
+    stylus.define('fx_devices', function(device,param){
       d = $config['devices'].vals[ id.val ];
       return d; 
     });
 
 
     stylus.define('fx_make_device_iter', function(len){
+
       const gen = makeDeviceGenerator()
       let i = gen(len.val);
+
+      if( typeof i === 'function' ){
+        let restart = i;
+        restart();
+        i = gen();
+      } 
+
       return i;
     });
 
-    stylus.define('fx_device_meta', function(k) {
-      return 1;
+
+    stylus.define('fx_device_size', function(device,getVals=false){
+      let $val;
+      device  = device.val
+      getVals = getVals.val;
+      if( device ){
+        $val  = ( getVals ? $devices[device]['range'] : $devices[device]['alt'] );
+        return ($val||null); 
+      }
+      return null;
     });
 
 
 
-    stylus.define('fx_make_size_iter', function(len) {
+    stylus.define('fx_make_size_iter', function(len){
+
       const gen = makeSizeGenerator()
       let i = gen(len.val);
+
+      if( typeof i === 'function' ){
+        let restart = i;
+        restart();
+        i = gen();
+      } 
+
       return i;
     });
 
 
-    stylus.define('fx_size_meta', function(param) {
+    stylus.define('fx_size_meta', function(param){
       $meta = $sizes['meta'];
       let lookup = param.val;
       let $val   = $meta[lookup] || null;
