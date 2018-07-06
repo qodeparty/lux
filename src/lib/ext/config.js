59ce162d2da05af172e1f21a1c490df13d33934b
qodebot <30004919+qodebot@users.noreply.github.com>
2018-07-06 15:15:54 +0000

    add show/hide devices, fix ultra

--- src/lib/ext/config.js
+++ src/lib/ext/config.js
@@ -3,7 +3,7 @@ $config = require('../defs/lux.json');
 $flags  = $config['flags'];
 $vals   = $config['init'];
 
-$devices   = $config['devices']['vals'];
+
 $sizes     = $config['sizes'];
 $size_lbls = $sizes['meta']['labels'];
 
@@ -23,60 +23,7 @@ function * listGenerator(iter){
 }
 
 
-//console.log(deviceGenerator)
-var deviceGen = listGenerator($devices);
 
-function makeDeviceGenerator(){
-  const len =  Object.keys($devices).length;
-  function restart(){
-    console.log('restarting generator...');
-    deviceGen = listGenerator($devices);
-  }
-  function generate(getLength){
-    if(getLength===1) return len;
-    if(!deviceGen)    return restart;
-    let $this = deviceGen.next();
-    if( !$this.done ) return $this.value; 
-    if( $this.done ){
-      console.error('generator done, sending restart()');
-      return restart;
-    }
-  }
-  return generate; 
-}
-
-
-
-var sizeGen = listGenerator($size_lbls);
-
-function makeSizeGenerator(){
-  const len =  Object.keys($size_lbls).length;
-  function restart(){
-    //console.log('restarting generator...');
-    sizeGen = listGenerator($size_lbls);
-  }
-  function generate(getLength){
-    if(getLength===1) return len;
-    if(!deviceGen)    return restart;
-    let $this = sizeGen.next();
-    if( !$this.done ) return ""+$this.value; 
-    if( $this.done ){
-      //console.error('generator done, sending restart()');
-      return restart;
-    }
-  }
-
-
-  return generate; 
-
-  // return function(getLength){
-  //   //console.log('len',(getLength?))
-  //   if(getLength===1) return len;
-  //   let $this = sizeGen.next();
-  //   if( ! $this.done ) return ""+$this.value; 
-  //   return null;
-  // }
-}
 
 
 const percGen = listGenerator($percs);
@@ -127,55 +74,8 @@ module.exports = () => {
     });
 
 
-    stylus.define('fx_devices', function(device,param){
-      d = $config['devices'].vals[ id.val ];
-      return d; 
-    });
 
 
-    stylus.define('fx_make_device_iter', function(len){
-
-      const gen = makeDeviceGenerator()
-      let i = gen(len.val);
-
-      if( typeof i === 'function' ){
-        let restart = i;
-        restart();
-        i = gen();
-      } 
-      return i;
-    });
-
-
-    stylus.define('fx_device_size', function(device,getVals=false){
-      let $val;
-      
-      device  = device.string
-      getVals = getVals.val;
-      //console.log($devices,device)
-
-      if( device ){
-        $val  = ( getVals ? $devices[device]['range'] : $devices[device]['alt'] );
-        return ($val||null); 
-      }
-      return null;
-    });
-
-
-
-    stylus.define('fx_make_size_iter', function(len){
-
-      const gen = makeSizeGenerator()
-      let i = gen(len.val);
-
-      if( typeof i === 'function' ){
-        let restart = i;
-        restart();
-        i = gen();
-      } 
-
-      return i;
-    });
 
 
     stylus.define('fx_size_meta', function(param){
--- src/lib/ext/devices.js
+++ src/lib/ext/devices.js
@@ -5,16 +5,119 @@ cyan  = process.env['COLOR_CYAN'];
 green = process.env['COLOR_GREEN'];
 end   = process.env['COLOR_NONE'];
 
+
+function * listGenerator(iter){
+  let index = 0;
+  let names = (Array.isArray(iter)) ? iter : Object.keys(iter);
+  for(d in names){
+    yield names[d];
+  }
+}
+
+
+//console.log(deviceGenerator)
+var deviceGen = listGenerator($devices);
+
+function makeDeviceGenerator(){
+  const len =  Object.keys($devices).length;
+  function restart(){
+    //console.log('restarting generator...');
+    deviceGen = listGenerator($devices);
+  }
+  function generate(getLength){
+    if(getLength===1) return len;
+    if(!deviceGen)    return restart;
+    let $this = deviceGen.next();
+    if( !$this.done ) return $this.value; 
+    if( $this.done ){
+      //console.error('generator done, sending restart()');
+      return restart;
+    }
+  }
+  return generate; 
+}
+
+
+var sizeGen = listGenerator($size_lbls);
+
+function makeSizeGenerator(){
+  const len =  Object.keys($size_lbls).length;
+  function restart(){
+    //console.log('restarting generator...');
+    sizeGen = listGenerator($size_lbls);
+  }
+  function generate(getLength){
+    if(getLength===1) return len;
+    if(!deviceGen)    return restart;
+    let $this = sizeGen.next();
+    if( !$this.done ) return ""+$this.value; 
+    if( $this.done ){
+      //console.error('generator done, sending restart()');
+      return restart;
+    }
+  }
+
+
+  return generate; 
+
+}
+
+
+
 module.exports = () => {
 
 
   return function(stylus){
+
 
     stylus.define('fx_devices', function(device,param){
       d = $config['devices'].vals[ id.val ];
       return d; 
     });
 
+
+    stylus.define('fx_make_device_iter', function(len){
+
+      const gen = makeDeviceGenerator()
+      let i = gen(len.val);
+
+      if( typeof i === 'function' ){
+        let restart = i;
+        restart();
+        i = gen();
+      } 
+      return i;
+    });
+
+
+    stylus.define('fx_device_size', function(device,getVals=false){
+      let $val;
+      
+      device  = device.string
+      getVals = getVals.val;
+      //console.log($devices,device)
+
+      if( device ){
+        $val  = ( getVals ? $devices[device]['range'] : $devices[device]['alt'] );
+        return ($val||null); 
+      }
+      return null;
+    });
+
+
+    stylus.define('fx_make_size_iter', function(len){
+
+      const gen = makeSizeGenerator()
+      let i = gen(len.val);
+
+      if( typeof i === 'function' ){
+        let restart = i;
+        restart();
+        i = gen();
+      } 
+
+      return i;
+    });
 
     stylus.define('fx_device_ranges', function(){
 
