56882785f72346857f5dec66d990ba673c92edd1
qodebot <30004919+qodebot@users.noreply.github.com>
2018-07-19 04:18:01 +0000

    add hex converter for icons

---
+++ src/lib/ext/hex-pad.js
@@ -0,0 +1,10 @@
+module.exports = () => {
+  return function(stylus){
+    stylus.define('fx_hex_pad', function($num,$len=3){
+    	$num = ($num.val).toString(16);
+    	let $s = String($num);
+    	while ($s.length < ($len || 2)) {$s = "0" + $s;}
+      return $s;
+    });
+  }
+}
